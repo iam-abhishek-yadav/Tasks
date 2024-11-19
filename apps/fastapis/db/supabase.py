359af3aabaa179adbd77865df54b608bb3b38c8e
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-11-19 22:01:11 +0530

    added authenticated routes sign-up,sign-in ,root

--- apps/fastapis/db/supabase.py
+++
@@ -1,8 +0,0 @@
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
-load_dotenv()
-url = os.getenv('SUPABASE_SUPAFAST_URL')
-key = os.getenv('SUPABASE_SUPAFAST_KEY')
-
-supabase: Client = create_client(url, key)
